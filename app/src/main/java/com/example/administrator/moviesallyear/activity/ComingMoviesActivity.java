5ef49f14c6790bc90ddc6d89c3a3f65b8886124d
andy <andygo520@163.com>
2017-03-27 18:13:30 +0800

    some changes of UI

---
+++ app/src/main/java/com/example/administrator/moviesallyear/activity/ComingMoviesActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.moviesallyear.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.moviesallyear.R;
+
+public class ComingMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coming_movies);
+    }
+}
