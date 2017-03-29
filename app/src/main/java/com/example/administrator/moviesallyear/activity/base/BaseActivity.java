0727f7bcc452952ef5b692e7af5cae86c73a3fb8
andy <andygo520@163.com>
2017-03-29 17:54:48 +0800

    some changes of UI

---
+++ app/src/main/java/com/example/administrator/moviesallyear/activity/base/BaseActivity.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2015 Drakeet <drakeet.me@gmail.com>
+ *
+ * This file is part of Meizhi
+ *
+ * Meizhi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Meizhi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Meizhi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.example.administrator.moviesallyear.activity.base;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.example.administrator.moviesallyear.DoubanApi;
+import com.example.administrator.moviesallyear.GankApi;
+import com.example.administrator.moviesallyear.QuanysFactory;
+
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+
+public class BaseActivity extends AppCompatActivity {
+
+    public static final GankApi sGankIO = QuanysFactory.getGankSingleton();
+    public static final DoubanApi sDoubanIO= QuanysFactory.getDoubanSingleton();
+
+    private CompositeSubscription mCompositeSubscription;
+
+
+    public CompositeSubscription getCompositeSubscription() {
+        if (this.mCompositeSubscription == null) {
+            this.mCompositeSubscription = new CompositeSubscription();
+        }
+
+        return this.mCompositeSubscription;
+    }
+
+
+    public void addSubscription(Subscription s) {
+        if (this.mCompositeSubscription == null) {
+            this.mCompositeSubscription = new CompositeSubscription();
+        }
+
+        this.mCompositeSubscription.add(s);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (this.mCompositeSubscription != null) {
+            this.mCompositeSubscription.unsubscribe();
+        }
+    }
+}
